29c490270c11a3d74f229927b5573ef8b281e773
yamamoto-yohei <yamamoto.yohei@dena.jp>
2018-11-28 06:25:17 +0900

    Revert "fix parameter of parseKeyList"
    This reverts commit 41f2058fb67db100b3dee4bd4c328278070c640e.

--- parser.go
+++ parser.go
@@ -71,7 +71,7 @@ func (p *Parser) ParseEntity(ctx context.Context, entity Entity) (dsEntity datas
 			}
 			props = append(props, *prop)
 			if key == nil && name == p.kindData.Scheme.Key {
-				if key, err = p.parseKeyList(ctx, val); err != nil {
+				if key, err = p.parseKeyList(ctx, prop.Value); err != nil {
 					return
 				}
 			}
